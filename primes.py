fa9a47a8916e53e0eb3e7d699a245e6a730f6d20
Angelo <angelo_384@hotmail.com>
2016-10-19 19:12:38 -0500

    [ADD] primes algorithm added

--- primes.py
+++ primes.py
@@ -1,16 +1,19 @@
 """
-Your module documentation here
+A class used for check if a number is prime or not
+author: Angelo Loza
+date: 19/10/2016
+for Vauxoo test.
 """
-
-
-class PrimeClass(object):
+class prime_class(object):
+    n = int(input("Insert number to check")) #Capture number
     """
-    Your class documentation here
+    Function who recieve a n number
     """
+    def is_prime(n):
+        for x in range(2, n): # Cycle for compare the module of the number by themself
+            if (n%x) == 0:# if the module is zero, the number is not prime
+                print "is not prime"
+                return False
+    return True
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+
