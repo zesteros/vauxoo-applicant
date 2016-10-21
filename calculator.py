5763d859d4d662515a167f706d1dab05e7251273
Angelo <angelo_384@hotmail.com>
2016-10-21 17:58:11 -0500

    [FIX] fixed indented

--- calculator.py
+++ calculator.py
@@ -7,7 +7,6 @@ A module used for sum a list of numbers for Vauxoo test.
 
 class CalculatorClass(object):
     """Can recieve a list of numbers and sum them."""
-
     def sum(self, num_list):
         """This function return the sum of every element of the list"""
         result = 0
