9761a3b3efa239169cca6b0a3958d6e5eab125c6
Angelo <angelo_384@hotmail.com>
2016-10-21 16:55:00 -0500

    [FIX] Logic and style fixed

--- primes.py
+++ primes.py
@@ -1,19 +1,16 @@
+"""PrimeClass
+
+A module used for check if a number is prime or not for Vauxoo test.
+
 """
-A class used for check if a number is prime or not
-author: Angelo Loza
-date: 19/10/2016
-for Vauxoo test.
-"""
-class prime_class(object):
-    n = int(input("Insert number to check")) #Capture number
-    """
-    Function who recieve a n number
-    """
-    def is_prime(n):
-        for x in range(2, n): # Cycle for compare the module of the number by themself
-            if (n%x) == 0:# if the module is zero, the number is not prime
-                print "is not prime"
+
+
+class PrimeClass(object):
+    """Function who recieve a n number"""
+
+    def is_prime(self, num_int):
+        """The function to know if a number is prime or not"""
+        for num in range(2, num):
+            if (num_int % num) == 0:
                 return False
-    return True
-
-
+        return True
