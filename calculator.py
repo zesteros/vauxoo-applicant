8258897e3c021d533ea405a25d274df5e1cfe4a5
Angelo <angelo_384@hotmail.com>
2016-10-21 17:52:34 -0500

    [FIX] Last bugs

--- calculator.py
+++ calculator.py
@@ -8,11 +8,9 @@ A module used for sum a list of numbers for Vauxoo test.
 class CalculatorClass(object):
     """Can recieve a list of numbers and sum them."""
 
-    num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
     def sum(self, num_list):
         """This function return the sum of every element of the list"""
         result = 0
-        for num in range(len(num_list)):
-            result = num_list[num] + num_list[num+1]
+        for num in num_list:
+            result += num
         return result
