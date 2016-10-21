d67699b19f10cf8318efedcc659d0240db53041f
Angelo <angelo_384@hotmail.com>
2016-10-21 16:55:36 -0500

    [FIX] Logic and style fixed

--- calculator.py
+++ calculator.py
@@ -1,25 +1,18 @@
-"""
-A module used for sum a list of numbers
-author: Angelo Loza
-date: 19/10/2016
-for Vauxoo test.
+"""CalculatorClass
+
+A module used for sum a list of numbers for Vauxoo test.
+
 """
 
 
 class CalculatorClass(object):
-    """
-    This class when is instantiated, can recieve a list of numbers and sum them.
-    """
-    amount_number = int(input("How many numbers you want to sum?\n")) # Input of amount of numbers
-    num_list = [0]*amount_number # creation of the list
-    for i in range(len(num_list)): # fill the list
-        num_list[i] = int(input("Insert the number:\n")) #assign to the position of the list
-    print "suma:", sum(num_list) # print the result  
-    def sum(num_list):
-        """
-        This function return the sum of every element of the list
-        """
-        for i in range(len(num_list)):
-            result = num_list[i]+num_list[i+1]
-        return result
+    """Can recieve a list of numbers and sum them."""
 
+    num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    def sum(self, num_list):
+        """This function return the sum of every element of the list"""
+        result = 0
+        for num in range(len(num_list)):
+            result = num_list[num] + num_list[num+1]
+        return result
